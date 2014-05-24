2afcdc3861f4500721e0569b2b340036d42f99c1
Simon Bengtsson <simonbengt@gmail.com>
2014-05-24 14:14:52 +0200

    Init with wall collisions almost working

---
+++ app.js
@@ -0,0 +1,130 @@
+var canvas;
+var ctx;
+
+$(document).ready(function () {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    resizeCanvas();
+    var world = new World();
+    setInterval(world.update, 5);
+});
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+/**
+ * Ball model
+ */
+function Ball(color) {
+
+    const RADIUS = 20;
+    const MASS = 20;
+    const INIT_SPEED_X = 0;
+    const INIT_SPEED_Y = 2;
+
+    // Initialize
+    this.color = color;
+    this.mass = MASS;
+    this.radius = RADIUS;
+    this.x = Math.floor(Math.random() * canvas.width);
+    this.y = Math.floor(Math.random() * canvas.height);
+    this.nextX = this.x;
+    this.nextY = this.y;
+    this.speedX = INIT_SPEED_X;
+    this.speedY = INIT_SPEED_Y;
+
+    /**
+     * Updates potential next position from speed
+     */
+    this.update = function () {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        this.nextX = this.x;
+        this.nextY = this.y;
+    };
+
+    /**
+     * Check and update ball position if wall collision
+     */
+    this.wallCollision = function () {
+
+        // Top
+        if (this.nextX - this.radius < 0) {
+            this.speedX = this.speedX * (-1);
+            this.nextX = this.radius;
+        }
+
+        // Right
+        else if (this.nextX + this.radius > canvas.width) {
+            this.speedX = this.speedX * (-1);
+            this.nextX = canvas.width - this.radius;
+        }
+
+        // Bottom
+        else if (this.nextY + this.radius > canvas.height) {
+            this.speedY = this.speedY * (-1);
+            this.nextY = canvas.height - this.radius;
+        }
+
+        // Left
+        else if (this.nextY - this.radius < 0) {
+            this.speedY = this.speedY * (-1);
+            this.nextY = this.radius;
+        }
+
+    };
+
+    /**
+     * Draw ball to canvas
+     */
+    this.draw = function () {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.fill();
+    }
+}
+
+/**
+ * World model
+ */
+function World() {
+
+    const BALL_COUNT = 10;
+    const GRAVITY = 5;
+
+    var balls = [];
+
+    // Create and place balls. Only add if they don't overlap any other
+    for (var i = 0; i < BALL_COUNT; i++) {
+        var color = i % 2 === 0 ? '#fff' : '#000';
+        var tmpBall = new Ball(color);
+        balls.push(tmpBall);
+    }
+
+
+    /**
+     * All world updates through here
+     */
+    this.update = function () {
+        resizeCanvas();
+        ballCollisions();
+        balls.forEach(function (ball) {
+            ball.update();
+            ball.wallCollision();
+        });
+        balls.forEach(function (ball) {
+            ball.draw();
+        });
+    };
+
+    /**
+     * Check if any ball has collided with any other and if so update appropriately
+     */
+    function ballCollisions() {
+        // TODO
+    }
+}
